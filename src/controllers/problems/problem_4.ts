39ef03348c22f44a2f82ec7ca655c7d4911a0e11
landienzla <landienzla@gmail.com>
2023-12-23 12:30:55 +0300

    problem4 pagination

--- src/controllers/problems/problem_4.ts
+++ src/controllers/problems/problem_4.ts
@@ -4,6 +4,9 @@ import { Response } from "express";
 import { Review as ReviewModel } from "../../models/review";
 
 export default async (req: RequestWithUser, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const pageSize = parseInt(req.query.pageSize as string) || 20;
+  const skip = (page - 1) * pageSize;
   // Restoranlara yapılan yorumlar baz alınarak, son yorum yapan 20 erkek kullanıcıyı yaşa göre sıralayınız. (Bir sonraki sorguda sonraki 20 erkek listelenecek şeklinde yorumlayıp sorguyu yazmanız beklenmektedir)
   const result = await ReviewModel.aggregate([
     {
@@ -26,12 +29,27 @@ export default async (req: RequestWithUser, res: Response) => {
         lastReviewDate: { $first: "$reviewDate" },
       },
     },
-    { $sort: { age: 1 } },
-    { $skip: 0 }, // offset // 0 dan başla
-    { $limit: 20 }, // limit  // 20 erkek kullanıcı
+    {
+      $facet: {
+        paginatedResults: [
+          { $sort: { age: 1 } },
+          { $skip: skip },
+          { $limit: pageSize },
+        ],
+        totalCount: [{ $count: "count" }],
+      },
+    },
+    // { $sort: { age: 1 } },
+    // { $skip: skip }, // offset // 0 dan başla
+    // { $limit: pageSize }, // limit  // 20 erkek kullanıcı
   ]);
   return sendResponse(req, res, 200, {
     message: "success",
-    result,
+    result: result?.[0]?.paginatedResults,
+    pagination: {
+      totalCount: result?.[0]?.totalCount?.[0]?.count,
+      page,
+      pageSize,
+    },
   });
 };
